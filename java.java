e58a35c72e16007ab00de56275793683bc3ebc5c
EshaanGulia <52140896+EshaanGulia@users.noreply.github.com>
2019-12-28 09:15:22 -0600

    Add files via upload

---
+++ java.java
@@ -0,0 +1,17 @@
+//author-Eshaan Gulia
+//date-12/28/2019
+//this code shows the % operator 
+
+
+class java{
+    public static void main(String args[] )    {
+        int x = 42;
+        double y = 42.25;
+
+
+        System.out.println("x mod 10 = " +x %10);
+        System.out.println("Y mod 10 = " + y % 10);
+
+
+    }
+}
